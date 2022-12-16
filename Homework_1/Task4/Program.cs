5768e2334733afc3b9c25f5512963d079a92157e
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2022-12-16 16:23:00 +0300

    Task4 solved

--- Homework_1/Task4/Program.cs
+++ Homework_1/Task4/Program.cs
@@ -4,3 +4,27 @@
 
 5 -> 2, 4
 8 -> 2, 4, 6, 8 */
+
+using static System.Console;
+Clear();
+
+Write("Enter number: ");
+int num = int.Parse(ReadLine()!);
+
+int count = 1;
+while (count <= num)
+{
+if (count % 2 == 0)
+    if (count == num)
+    {
+        Write(count);
+    }
+    else if (count == num - 1)
+    {
+        Write(count);
+    }
+    else {
+        Write($"{count}, ");
+    }
+    count++;
+}
