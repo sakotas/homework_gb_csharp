ff9a1be139ecaa0e1274e395e09e91910efc23eb
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2022-12-16 12:59:44 +0300

    Clear(); command added to Program.cs

--- Homework_1/Task1/Program.cs
+++ Homework_1/Task1/Program.cs
@@ -7,7 +7,7 @@ a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 */
 
 using static System.Console;
-
+Clear();
 Write("Enter first number: ");
 int a = int.Parse(ReadLine()!);
 Write("Enter second number: ");
