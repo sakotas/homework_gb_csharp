118b717d89ede4e6f673780d0f26dd1c8d3657e9
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2022-12-20 20:08:38 +0300

    Task 13 solved

--- Homework_2/Task13/Program.cs
+++ Homework_2/Task13/Program.cs
@@ -3,3 +3,23 @@
 // 645 -> 6
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+using static System.Console;
+Clear();
+
+WriteLine("Input three-digit number: ");
+int num = int.Parse(ReadLine()!);
+
+if(num > 99)
+{
+    int a = num%1000/100;
+    WriteLine(a);
+}
+else 
+{
+    WriteLine("третьей цифры нет");
+}
+
+// You can use short oneline command
+// Uncomment line 25 and comment lines 13-21
+// WriteLine(num > 99?num%1000/100:"третьей цифры нет");
