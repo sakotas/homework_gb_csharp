4758ad35e449c1f46db71c6327b92e51c5a4c1f3
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2022-12-20 20:19:49 +0300

    Task 15 solved

--- Homework_2/Task15/Program.cs
+++ Homework_2/Task15/Program.cs
@@ -4,3 +4,26 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+using static System.Console;
+Clear();
+
+WriteLine("Input one-digit number from 1 to 7: ");
+int num = int.Parse(ReadLine()!);
+
+if (num < 1 || num > 7)
+{
+    WriteLine($"You entered wrong number {num} is out of range from 1 to 7");
+}
+else if (num == 6 || num == 7)
+{
+    WriteLine("Да");
+}
+else
+{
+    WriteLine("Нет");
+}
+
+// You can use short oneline command
+// Uncomment line 29 and comment lines 13-25
+// WriteLine((num < 1) || (num > 7)?$"You entered wrong number {num} is out of range from 1 to 7":(num == 6) || (num == 7)?"Да":"Нет");
