6f5661ac0fec554df1847371de5d2c2955c9665c
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2022-12-23 13:40:00 +0300

    Task19 solved

--- Homework_3/Task19/Program.cs
+++ Homework_3/Task19/Program.cs
@@ -3,5 +3,29 @@
 // 12821 -> да
 // 23432 -> да
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using static System.Console;
+Clear();
+
+Write("input 5-digit number:");
+if (!int.TryParse(ReadLine(), out int number))
+
+{
+    WriteLine("Error. Input number, not string");
+    return;
+}
+
+if (number < 10000 || number > 99999)
+{
+    WriteLine($"Error. Insert 5-digit number");
+    return;
+}
+
+else
+{
+    string n = number.ToString();
+    if (n[0] == n[4] || n[1] == n[3])
+    {
+        Console.WriteLine($"Ваше число: {n} - палиндром.");
+    }
+    else Console.WriteLine($"Ваше число: {n} - НЕ палиндром.");
+}
