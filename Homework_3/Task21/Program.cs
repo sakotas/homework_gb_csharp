ba46ee4c7913367bb261563c23962e59fcd63967
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2022-12-23 13:04:41 +0300

    Task 21 solved

--- Homework_3/Task21/Program.cs
+++ Homework_3/Task21/Program.cs
@@ -2,5 +2,50 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using static System.Console;
+Clear ();
+
+Write("input x1:");
+if (!int.TryParse(ReadLine(), out int X1))
+{
+    WriteLine("Error. Input number, not string");
+    return;
+}
+
+Write("input y1:");
+if (!int.TryParse(ReadLine(), out int Y1))
+{
+    WriteLine("Error. Input number, not string");
+    return;
+}
+
+Write("input z1:");
+if (!int.TryParse(ReadLine(), out int Z1))
+{
+    WriteLine("Error. Input number, not string");
+    return;
+}
+
+Write("input x2:");
+if (!int.TryParse(ReadLine(), out int X2))
+{
+    WriteLine("Error. Input number, not string");
+    return;
+}
+
+Write("input y2:");
+if (!int.TryParse(ReadLine(), out int Y2))
+{
+    WriteLine("Error. Input number, not string");
+    return;
+}
+
+Write("input z2:");
+if (!int.TryParse(ReadLine(), out int Z2))
+{
+    WriteLine("Error. Input number, not string");
+    return;
+}
+
+double distance = Math.Sqrt (Math.Pow((X2 - X1),2) +  Math.Pow((Y2 - Y1),2) + Math.Pow((Z2 - Z1),2));
+WriteLine ("Distanse between 2 points in 3-d dimension is {0:f2}", distance);
