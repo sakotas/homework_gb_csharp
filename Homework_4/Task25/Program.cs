ced10688d0a4f2c1f31fc3d5820b0249ecb3984a
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2023-01-17 12:58:11 +0300

    Task25 solved

--- Homework_4/Task25/Program.cs
+++ Homework_4/Task25/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+// Задача 25: Напишите программу, реализующую метод,который принимает на вход два числа (A и B) и возводит число A в натуральную степень B с использованием цикла.
+// В задаче не использвать стандартный метод Pow!
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+WriteLine("========НАЧАЛО========");
+Write("Введите числа А и Б через пробел: ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+WriteLine("======================");
+checkParsing(int.TryParse(parameters[0], out int A));
+checkParsing(int.TryParse(parameters[1], out int B));
+
+WriteLine("======================");
+WriteLine($"Число А: {A}");
+WriteLine($"Число Б: {B}");
+
+WriteLine("======================");
+WriteLine($"Число {A}, возведенное в натуральную степень {B}, дает {numbersPower(A, B)}");
+WriteLine("========КОНЕЦ========");
+
+void checkParsing(bool isParse)
+{
+    if (!isParse)
+    {
+        Console.WriteLine($"Проверка не пройдена - НЕ ЧИСЛО!");
+    }
+    else
+    {
+        Console.WriteLine($"Проверка пройдена");
+    }
+}
+
+int numbersPower(int a, int b)
+{
+    int result = 1;
+
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+
+    return result;
+}
