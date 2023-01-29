8132222cdb01de9c95ed4e44c3a4f3183fe5dedb
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2023-01-29 13:40:25 +0300

    Task 47 solved

--- Homework_7/Task47/Program.cs
+++ Homework_7/Task47/Program.cs
@@ -9,6 +9,38 @@ Clear();
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+WriteLine("Введите размер матрицы через пробел: ");
+int[] intParams=Array.ConvertAll(ReadLine()!.Split(new char[]{' '},StringSplitOptions.RemoveEmptyEntries),int.Parse);
 
+WriteLine($"m = {intParams[0]}, n = {intParams[1]}");
+double[,] array = GetRandomFloatMatrixArray(intParams[0], intParams[1]); 
+PrintMatrixArray(array);
 
+double[,] GetRandomFloatMatrixArray(int rows, int columns)
+{
+    Random random = new Random();
+    
+    double[,] resultArray = new double[rows, columns];
 
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultArray[i, j] = Math.Round((random.Next(-10,10) + random.NextDouble()),1);
+        }
+
+    }
+    return resultArray;
+}
+
+void PrintMatrixArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j], 4} ");
+        }
+        WriteLine();
+    }
+}
